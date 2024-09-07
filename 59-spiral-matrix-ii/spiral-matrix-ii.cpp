e925a6900b7d006b5b282e150b4109fa5daab4a2
Sravankumar-14 <142382521+Sravankumar-14@users.noreply.github.com>
2024-09-07 18:56:00 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (74.18%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> ans(n, vector<int>(n, -1));  // Initialize the matrix with -1
+        vector<vector<int>>ans(n,vector<int>(n,-1));
         int a = 1;
-        int total = n * n;
-        int i = 0, j = 0;
-        
-        while (a <= total) {
-            // Move right
-            while (a <= total && j < n) {
-                if (ans[i][j] != -1) break;
+        int total = n*n;
+        int i=0,j=0;
+        while(a<=total)
+        {
+            while(a<=total && j<n)
+            {
+                if(ans[i][j] != -1) break;
                 ans[i][j++] = a++;
             }
-            j--; i++;  // Adjust indices after breaking out
-            
-            // Move down
-            while (a <= total && i < n) {
-                if (ans[i][j] != -1) break;
+            j--;
+            i++;
+            while(a<=total && i<n)
+            {
+                if(ans[i][j] != -1) break;
                 ans[i++][j] = a++;
             }
-            i--; j--;
-            
-            // Move left
-            while (a <= total && j >= 0) {  // Correct the condition here
-                if (ans[i][j] != -1) break;
+            i--;
+            j--;
+            while(a<=total && j>=0)
+            {
+               if(ans[i][j] != -1) break;
                 ans[i][j--] = a++;
             }
-            j++; i--;
-            
-            // Move up
-            while (a <= total && i >= 0) {  // Correct the condition here
-                if (ans[i][j] != -1) break;
+            j++;
+            i--;
+            while(a<=total && i>=0)
+            {
+                if(ans[i][j] != -1) break;
                 ans[i--][j] = a++;
             }
-            i++; j++;
+            i++;
+            j++;
         }
-        
         return ans;
     }
 };
