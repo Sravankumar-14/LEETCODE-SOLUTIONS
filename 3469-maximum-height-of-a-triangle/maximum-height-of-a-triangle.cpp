87c1371be8a577b696ea31db8d5ea24ae0264d88
Sravankumar-14 <142382521+Sravankumar-14@users.noreply.github.com>
2024-09-12 08:29:11 +0530

    Time: 6 ms (23.88%) | Memory: 8.3 MB (15.36%) - LeetSync

---
+++ 3469-maximum-height-of-a-triangle/maximum-height-of-a-triangle.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int calHeight(int red, int blue)
+    {
+        int level = 0;
+        int a = 1;
+        bool turn = true;
+        while(1)
+        {
+            level ++;
+            if(turn)
+            {
+                if(red >= a) red -= a;
+                else break;
+            }
+            else
+            {
+                if(blue >= a) blue -= a;
+                else break;
+            }
+            a++;
+            turn = !turn;
+        }
+        return level;
+    }
+    int maxHeightOfTriangle(int red, int blue) {
+        int count1 = calHeight(red,blue);
+        int count2 = calHeight(blue,red);
+
+        cout<<count1;
+        return max(count1-1,count2-1);
+    }
+};
