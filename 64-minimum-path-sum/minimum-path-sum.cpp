cad7ef634ad223f6d08759d5e2c3312d76e23102
Sravankumar-14 <142382521+Sravankumar-14@users.noreply.github.com>
2024-10-10 23:25:33 +0530

    Time: 10 ms (63.42%) | Memory: 14.9 MB (19.86%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int smallPath(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp)
+    int smallPath(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& grid)
     {
-        if(m == 0 && n == 0)
-        {
-            return grid[m][n];
-        }
-        if(m < 0 || n < 0) 
-        {
-            return INT_MAX;
-        }
-        if(dp[m][n] != INT_MAX) return dp[m][n];
-        int up = smallPath(m-1,n,grid,dp);
-        int left = smallPath(m,n-1,grid,dp);
-        return dp[m][n] = grid[m][n] + min(up,left);
+        if(i == 0 && j == 0) return grid[0][0];
+        if(i < 0 || j < 0) return 1e9;
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = grid[i][j] + smallPath(i-1,j,dp,grid);
+        int left = grid[i][j] + smallPath(i,j-1,dp,grid);
+        return dp[i][j] = min(up,left);
     }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>dp(m, vector<int>(n,INT_MAX));
-        return smallPath(m-1,n-1,grid,dp);
+        vector<vector<int>>dp(m, vector<int>(n,-1));
+        return smallPath(m-1,n-1,dp,grid);
     }
 };
