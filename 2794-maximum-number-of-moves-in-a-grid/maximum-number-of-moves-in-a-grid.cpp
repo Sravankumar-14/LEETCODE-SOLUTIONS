52869ebce2a426d821e23e9ee618f45fb3d49004
Sravankumar-14 <142382521+Sravankumar-14@users.noreply.github.com>
2024-10-29 19:12:25 +0530

    Time: 4 ms (93.48%) | Memory: 74.5 MB (25.57%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
@@ -6,24 +6,23 @@ public:
         if(middle >= top && middle >= bottom) return middle;
         return bottom;
     }
-    int rec(vector<vector<int>>& grid, int row, int col, unordered_map<string,int>& dp, int& rowSize, int & colSize,int prev)
+    int rec(vector<vector<int>>& grid, int row, int col, vector<vector<int>>& dp, int& rowSize, int & colSize,int prev)
     {
         if(row < 0 || row >= rowSize || col >= colSize) return 0;
         if(col != 0 && grid[row][col] <= prev) return 0;
-        string str = to_string(row) +","+ to_string(col);
-        if(dp.find(str) != dp.end()) return dp[str];
+        if(dp[row][col] != -1) return dp[row][col];
 
         int top = rec(grid, row-1, col+1, dp, rowSize, colSize, grid[row][col]);
         int middle = rec(grid, row, col+1, dp, rowSize, colSize, grid[row][col]);
         int bottom = rec(grid, row+1, col+1, dp, rowSize, colSize, grid[row][col]); 
         int mx = maximum(top,middle,bottom);
-        dp[str] = mx;
-        return dp[str] + 1;
+        dp[row][col] = mx;
+        return dp[row][col] + 1;
     }
     int maxMoves(vector<vector<int>>& grid) {
         int rowSize = grid.size();
         int colSize = grid[0].size();
-        unordered_map<string, int>dp;
+        vector<vector<int>>dp(rowSize,(vector<int>(colSize,-1)));
         int maxMoves = 0;
         for(int i=0; i<rowSize; i++)
         {
