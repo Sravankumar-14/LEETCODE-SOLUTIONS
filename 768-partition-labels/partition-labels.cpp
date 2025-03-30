066824c8d7245a5687048c8f456e754da40c323a
Sravankumar-14 <142382521+Sravankumar-14@users.noreply.github.com>
2025-03-30 10:56:08 +0530

    Time: 8 ms (7.00%) | Memory: 8.9 MB (71.56%) - LeetSync

---
+++ 768-partition-labels/partition-labels.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        map<char, int> last;
+        vector<int>ans;
+        for(int i=0; i<s.size(); i++)
+        {
+            last[s[i]] = i;
+        }
+        for(auto mp : last) cout<<mp.first<<"-"<<mp.second<<endl;
+        int it = 0;
+        int windowSize = last[s[0]];
+        int count = 0;
+        while(it < s.size())
+        {
+            for(; it<=windowSize; it++)
+            {
+                if(last[s[it]] > windowSize) windowSize = last[s[it]];
+                count ++;
+            }
+            ans.push_back(count);
+            count = 0;
+            if(it < s.size()) windowSize = last[s[it]];
+        }
+        return ans;
+    }
+};
