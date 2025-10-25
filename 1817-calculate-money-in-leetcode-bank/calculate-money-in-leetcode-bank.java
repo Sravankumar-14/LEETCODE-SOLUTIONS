59428a73730297250a383cca67765739ca267baf
Sravankumar-14 <142382521+Sravankumar-14@users.noreply.github.com>
2025-10-25 23:11:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (58.30%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -3,15 +3,15 @@ class Solution {
         int quotient = n / 7;
         int rem = n % 7;
         int ans = 0;
-        System.out.println(quotient+" "+ rem);
+        // System.out.println(quotient+" "+ rem);
         for(int i=0; i<=quotient; i++) {
             ans += (28 + (7*i));
         }
-        System.out.println(ans);
+        // System.out.println(ans);
         int a = 28 + (7*quotient);
         int b = ((rem * (rem + 1)) / 2) + (rem * quotient);
         int trim = a - b;
-        System.out.println(trim);
+        // System.out.println(trim);
         ans -= trim;
         return ans;
     }
